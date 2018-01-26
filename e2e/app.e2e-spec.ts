1585b6239bc583d7d3b99b723b5d6712293e6421
James Duffield <james.h.duffield@gmail.com>
2018-01-26 13:36:53 +1100

    Initial angular 2 commit

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { AppPage } from './app.po';
+
+describe('tomb-of-annihilation App', () => {
+  let page: AppPage;
+
+  beforeEach(() => {
+    page = new AppPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  });
+});
